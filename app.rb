32c0df944c4bbb727cabbddb4946b2f2138cb229
Erlend Finvåg <erlend@users.noreply.github.com>
2022-10-01 07:45:30 +0200

    Fix formatting in browsers

--- app.rb
+++ app.rb
@@ -2,10 +2,11 @@
 require "sinatra"
 
 get "/" do
+  new_line = request.env["HTTP_ACCEPT"].include?("text/html") ? "<br>" : "\n"
   stream do |out|
     request.each_header do |raw_key, value|
       key = raw_key.split("_")[1..].map(&:capitalize).join("-")
-      out << "#{key}: #{value}\n" if key.start_with?("X-Webauth")
+      out << "#{key}: #{value}#{new_line}" if key.start_with?("X-Webauth")
     end
   end
 end
